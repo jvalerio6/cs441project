a9293f37bf623ed2b32e77102194c82db964e633
Javier <jvalerio6@gmail.com>
2016-03-29 10:08:41 -0700

    Implementing Project Manager class

--- src/EisenhowerBox/ProjectManager.java
+++ src/EisenhowerBox/ProjectManager.java
@@ -1,36 +1,45 @@
 package EisenhowerBox;
 
 import java.util.Date;
+import java.util.List;
 
 public class ProjectManager extends User {
-	String member_first, member_last, member_username;
+
+	private List<Project> project_list;
+	private Project project;
 
 	public ProjectManager(String member_first, String member_last, String member_username) {
 		super(member_first, member_last, member_username);
 	}
 
 	public Project createProject() {
-		return null;
+		project = new Project();
+
+		return project;
 	}
 
 	public Project createProject(String project_name) {
-		return null;
+		project = new Project(project_name);
+
+		return project;
 	}
 
-	public Project createProject(String project_name, String priority, Date start_date, Date end_date) {
-		return null;
+	public Project createProject(String project_name, String importance, Date start_date, Date end_date) {
+		project = new Project(project_name, start_date, end_date, Project.Utile.Importance.valueOf(importance) );
+
+		return project;
 	}
 
-	public Date setStartDate(Date start_date) {
-		return null;
+	public void setStartDate(Date start_date) {
+		project.setStrtDate(start_date);
 	}
 
-	public Date setEndDate(Date end_date) {
-		return null;
+	public void setEndDate(Date end_date) {
+		project.setStrtDate(end_date);
 	}
 
 	boolean isWorkingOnProject(Project project) {
-		return false;
+		return (this.project_list.indexOf(project)) >= 0 ? true : false;
 	}
 
 }
